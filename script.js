6d956d6fd520a41f2db6bdb9acd21e622cf289db
Криницын Динар Олегович <dokrinits1@www.mts.ru>
2025-10-12 20:48:14 +0300

    Удалить весь код из ветки

--- script.js
+++
@@ -1,9 +0,0 @@
-    const title = "The first branch in Git";
-    const screens = 12345;
-    const screenPrice = 987654;
-    const rollback = 15;
-    const fullPrice = 1247532;
-    const adaptive = true;
-alert(`Привет, друг!`);
-
-console.log('Приветствую гостя сайта!')
