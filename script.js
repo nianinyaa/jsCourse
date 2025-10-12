9bbe5e7b4a9e6475e0fb85748d1989a78f85261f
Криницын Динар Олегович <dokrinits1@www.mts.ru>
2025-10-12 19:59:39 +0300

    добавила первый console.log после создания удаленного репозитория

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const div = document.getElementById('github')
 
 const logged = {
     console.log(div.textContent);
-    console.log('commits');
+    console.log(' commits');
 }
+
+console.log('Hello world')
