7f3461bda6f893fc677e20c3229783b5d0eb4190
Криницын Динар Олегович <dokrinits1@www.mts.ru>
2025-10-26 19:15:33 +0300

    Выполнено основное дз

--- script.js
+++ script.js
@@ -1,8 +1,71 @@
-const div = document.getElementById('github')
+let title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+const adaptive = prompt("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+const rollbackPercent = 15;
 
-const logged = {
-    console.log(div.textContent);
-    console.log(' commits');
+const getAllServicePrices = function(servicePrice1,servicePrice2){
+    return servicePrice1 + servicePrice2;};
+const allServicePrices = getAllServicePrices(servicePrice1,servicePrice2);
+
+
+
+function getFullPrice(screenPrice, allServicePrices){
+    return screenPrice + allServicePrices;}
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+
+const getTitle = function(title){
+    if (!title) return "";
+    const trimmedTitle = title.trim();
+    return trimmedTitle.charAt(0).toUpperCase() + trimmedTitle.slice(1).toLowerCase();
+} 
+ title = getTitle(title);
+
+
+const getServicePercentPrices = function (fullPrice, rollbackPercent){
+    const rollbackSum = fullPrice * (rollbackPercent/100);
+    return Math.round(fullPrice - rollbackSum)};
+const servicePercentPrice = getServicePercentPrices(fullPrice, rollbackPercent);
+
+
+
+
+ function getRollbackMessage(fullPrice){
+
+ if (fullPrice > 30000){
+     console.log("Даем скидку в 10%");
+ }
+ else if(fullPrice > 15000 && fullPrice <= 30000){
+     console.log("Даем скидку в 5%");
+ }
+
+else if (fullPrice <= 15000 && fullPrice >= 0){
+    console.log("Скидка не предусмотрена");
 }
 
-console.log('Hello world')
+else{
+    console.log("Что то пошло не так");
+}
+}
+
+
+ function showTypeOf(variable) {
+     console.log(typeof variable);
+ }
+
+
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log("Типы экранов для разработки:", screens);
+
+getRollbackMessage(fullPrice);
+
+console.log("Стоимость за вычетом отката:", servicePercentPrice);
